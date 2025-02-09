97f66930301b51477b5b9433496e19bce3290334
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2025-02-08 22:54:25 -0300

    atualizado

---
+++ Javascript/login.js
@@ -0,0 +1,9 @@
+var usuario = document.getElementById('usuario')
+var labelusuario = document.getElementById('labelusuario')
+
+var senha = document.getElementById('senha')
+var labelsenha = document.getElementById('labelsenha')
+
+function logar() {
+    alert('ola')
+}
